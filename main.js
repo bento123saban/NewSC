495ac22ccfa18fd0e65a5e6f0ce5add255f43426
bento123saban <bendhard16@gmail.com>
2025-05-30 09:12:52 +0900

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+                                                                                                                                                                        
+
+const navButton = document.querySelector('.nav-button')
+
+
+
+navButton.onclick = function (e) {
+    e.target.classList.toggle('active')
+}
